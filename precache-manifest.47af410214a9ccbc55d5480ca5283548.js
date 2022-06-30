0274bd8b4af516c23732adb577bcd8a5080bdebd
Code Sandbox <hello@codesandbox.io>
2022-06-30 15:11:18 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.47af410214a9ccbc55d5480ca5283548.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27f9cbcf4052e7581b919cb03c0f5a42",
+    "url": "https://geetesh2.github.io/csb-61osen/index.html"
+  },
+  {
+    "revision": "9302e28affcbd310209f",
+    "url": "https://geetesh2.github.io/csb-61osen/static/js/2.17e1ab02.chunk.js"
+  },
+  {
+    "revision": "8647bc2942f5e2e662a0",
+    "url": "https://geetesh2.github.io/csb-61osen/static/js/main.879f122a.chunk.js"
+  },
+  {
+    "revision": "2bdbea9dbadc0ac18332",
+    "url": "https://geetesh2.github.io/csb-61osen/static/js/runtime-main.e57e0530.js"
+  }
+]);
